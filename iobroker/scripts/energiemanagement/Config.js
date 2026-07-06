bd5f08855c9c61cc1259bc4f28b2e9755a0232a8
Starter4711 <15359915+Starter4711@users.noreply.github.com>
2026-07-06 13:14:43 +0200

    Configure house grid meter alias

--- iobroker/scripts/energiemanagement/Config.js
+++ iobroker/scripts/energiemanagement/Config.js
@@ -8,7 +8,7 @@
 const ROOT = '0_userdata.0.Energiemanagement';
 
 const states = [
-    [`${ROOT}.Config.Zaehlpunkt_Haus_Leistung_ID`, '', 'string', 'state'],
+    [`${ROOT}.Config.Zaehlpunkt_Haus_Leistung_ID`, 'alias.0.EM24 New Grid.Power', 'string', 'state'],
     [`${ROOT}.Config.Zaehlpunkt_Halle_Leistung_ID`, 'alias.0.EM24 Hall Grid.Power', 'string', 'state'],
     [`${ROOT}.Config.Zaehlpunkt_Alte_Wohnung_Leistung_ID`, '', 'string', 'state'],
     [`${ROOT}.Config.Vorzeichen_Netzbezug`, 1, 'number', 'value'],
